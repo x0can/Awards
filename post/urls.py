0dac1c02367ed8978b7f8f370c51ae226b6e0d34
alexmwaura <alexmwaura43@gmail.com>
2019-11-25 09:15:26 +0300

    modify app routes

--- post/urls.py
+++ post/urls.py
@@ -1,11 +1,31 @@
 from . import views
-from django.urls import path
+from .views import *
+from rest_framework.authtoken.views import obtain_auth_token
+
+from django.urls import path,include
+
+
+post_list = PostViewSet.as_view({'get': 'list'})
+# user\_detail = UserViewSet.as\_view({'get': 'retrieve'})
+
+
+
+
+
+
+
+
+
+
+
 
 
 urlpatterns = [
-    path('post/<int:pk>/',views.PostDetailView.as_view(), name ='post-detail'),
+    path('detail/<int:pk>/',views.PostDetailView.as_view(), name ='post-detail'),
     path('post/new/',views.PostCreateView.as_view(), name ='post-create'),
-
+    path (r'^search/',views.search_results,name= 'search_results'),    
+    path('api-auth/',include('rest_framework.urls', namespace='rest_framework')),
+    path('post/review/<int:pk>/',views.rate, name ='post-review'),    
 
 
 ]
